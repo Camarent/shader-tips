88f38d14e5e2504bd77a56c19b58715c6f4ef774
Kirill Titov <ktitov@corp.nekki.ru>
2020-08-13 12:58:14 +0300

    Added World UV Projection for Object for HDRP

---
+++ hdrp-samples/Assets/Samples/World UV Projection for Object/Readme.md
@@ -0,0 +1,6 @@
+# World UV Projection
+## Purpose
+  - Useful for gradients
+  - You can kinda create a simple grid
+## Links
+Based on https://twitter.com/Patrickd3/status/1290671080852672512
---
+++ hdrp-samples/Assets/Samples/World UV Projection for Object/WorldUVProjection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Samples.World_UV_Projection_for_Object
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    [ExecuteInEditMode]
+    public class WorldUVProjection : MonoBehaviour
+    {
+        private Material _material;
+        private MeshRenderer _renderer;
+        
+        private static readonly int Scale = Shader.PropertyToID("_Scale");
+
+        void Start()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+            _material = _renderer.sharedMaterial;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_material != null)
+                _material.SetFloat(Scale, _renderer.bounds.extents.y);
+        }
+    }
+}
